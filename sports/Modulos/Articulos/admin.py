20cd48c2333ad8d673aa2e38b1a8931412ef79b9
Cuauhtémoc Gerardo Montoya Reséndez <144742771+Cuauhtemoc45@users.noreply.github.com>
2023-12-06 10:27:09 -0600

    Add files via upload

---
+++ sports/Modulos/Articulos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Modulos.Articulos.models import *
+# Register your models here.
+
+admin.site.register(admins)
+admin.site.register(articulos)
+admin.site.register(usuarios)
+
+
+    
